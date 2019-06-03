f5a9546b2bd804af42c7c37283f08bdc8938eeb8
yeldaraimakov <yeldar.aimakov@gmail.com>
2019-06-03 17:42:56 +0600

    bug fixed

--- portal/music_portal/urls.py
+++ portal/music_portal/urls.py
@@ -4,5 +4,5 @@ from portal.music_portal import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('/download/<int:music_id>/', views.download, name='download'),
+    path('download/<int:music_id>/', views.download, name='download'),
 ]
