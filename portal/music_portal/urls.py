07f5d998ce0f47a34760aeabe811092daedfda15
yeldaraimakov <yeldar.aimakov@gmail.com>
2019-06-04 01:38:40 +0600

    contacts, videos page added

--- portal/music_portal/urls.py
+++ portal/music_portal/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 
 from portal.music_portal import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('download/<int:music_id>/', views.download, name='download'),
+    path('videos/', TemplateView.as_view(template_name='music_portal/videos.html'), name='videos'),
+    path('contacts/', TemplateView.as_view(template_name='music_portal/contacts.html'), name='contacts'),
 ]
