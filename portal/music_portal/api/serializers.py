9411a9e6f380289b1e6bac50ff8ec90880f3d1f5
yeldaraimakov <yeldar.aimakov@gmail.com>
2019-06-18 23:33:30 +0600

    artist added to rest

--- portal/music_portal/api/serializers.py
+++ portal/music_portal/api/serializers.py
@@ -4,6 +4,8 @@ from portal.music_portal.models import Music
 
 
 class MusicListSerializer(serializers.ModelSerializer):
+    artist = serializers.CharField(source='artist.nick_name')
+
     class Meta:
         model = Music
         fields = ('id', 'name', 'genre', 'artist', 'audio', 'audio_len', 'word_author', 'music_author')
